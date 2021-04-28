59b6b96df0a968a3834b40a034c7e247568f31e5
ihab abadi <ihab@utopios.net>
2021-04-28 12:29:32 +0200

    exemple service upload

---
+++ exampleMVCApplication/Services/IUpload.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exampleMVCApplication.Services
+{
+    public interface IUpload
+    {
+        bool Upload(IFormFile[] files);
+    }
+}
---
+++ exampleMVCApplication/Services/UploadService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exampleMVCApplication.Services
+{
+    public class UploadService : IUpload
+    {
+        private IWebHostEnvironment _env;
+
+        public UploadService(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        public bool Upload(IFormFile[] files)
+        {
+            foreach(IFormFile file in files)
+            {
+                string path = Path.Combine(_env.WebRootPath, file.FileName);
+                Stream stream = File.Create(path);
+                file.CopyTo(stream);
+                stream.Close();
+            }
+            return true;
+        }
+    }
+}
